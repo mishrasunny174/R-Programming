2c9d51a14e5429b3ea95cd0aa0be8f6e6dac3231
Sunny Mishra <mishrasunny174@gmail.com>
2020-01-24 12:03:18 +0530

    some reordering

--- demos/graphs.R
+++ demos/graphs.R
@@ -12,3 +12,24 @@ ggplot(data = mpg, mapping = aes(x=hwy, y=cyl)) + geom_point()
 # Scatter plot using ggplot with engine displacement vs highway miles with class as color aesthetic
 ggplot(data = mpg, mapping = aes(x = displ, y = hwy, color = class)) + geom_point()
 
+# Facet wraps, to divide the graphs into subgraphs based on some class here the argument is a Formula which
+# is a data structure in R which defines which attribute to use to divide the graphs
+# it is declared like ~ attribute
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) + geom_point() + facet_wrap( ~ class)
+
+# Facet grid is used to facet the plot based on two variables.
+ggplot(data = mpg, mapping = aes(x = displ, y = hwy)) + geom_point() + facet_grid(drv ~ class)
+
+# geom_smooth is a geom to create smooth lines.
+ggplot(dat = mpg, mapping = aes(x=displ, y=hwy)) + geom_smooth()
+
+# geom_smooth also accepts aesthetics to draw different lines
+
+ggplot(dat = mpg, mapping = aes(x=displ, y=hwy)) + geom_smooth(mapping = aes(linetype=drv))
+ggplot(dat = mpg, mapping = aes(x=displ, y=hwy)) + geom_smooth(mapping = aes(color=drv))
+
+# we can overlay geoms on top of each other to display more information
+
+ggplot(dat = mpg, mapping = aes(x=displ, y=hwy)) + geom_point() + geom_smooth()
+
+
